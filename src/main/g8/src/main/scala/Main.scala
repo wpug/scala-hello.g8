585aa0708be0660e8f13e6926f0a52547ca62c86
wpug <w.pawlowski@inf.ug.edu.pl>
2020-10-06 00:15:47 +0200

    Update Main.scala

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,3 +1,16 @@
 object Main extends App {
-  println("Hello, World!")
+  println("Jeśli koniecznie chcemy, to w Scali możemy też pisać w stylu XIX-wiecznym:")
+  print("Podaj wartość y: ")
+  // Wczytujemy liczbę całkowitą i zapamiętujemy ją jako „wartość” max
+  // Użycie wartości zamiast zmiennej to drobne odstępstwo od stylu XIX-wiecznego ;)
+  val max = io.StdIn.readInt()
+  // deklarujemy i inicjalizujemy zmienną x
+  var x = 1
+  // drobna próbka „interpolacji” w wypisywanym napisie
+  println(s"Liczby z przedziału [1..$max] to:")
+  // w „pętli” zmieniamy wartość zmiennej aż będzie ona równa „max”
+  while (x <= max) {
+    println(x)
+    x += 1
+  }
 }
